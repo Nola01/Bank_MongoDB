31c379d8ad87752994edb447360adb50dee1ebec
nola01 <juanmanolasco0105@gmail.com>
2021-06-03 11:06:19 +0200

    Conexión completa

--- src/modelo/Conexion.java
+++ src/modelo/Conexion.java
@@ -1,31 +1,18 @@
 package modelo;
 
 import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+
 
 public class Conexion {
     //Patron Singleton
     private Connection connection;
     private static Conexion conexion;
-
-    /*public static void pruebaConexion () {
-        MongoClient mongo = new MongoClient("localhost", 27017);
-        *//*MongoCredential mongoCredential = MongoCredential.createCredential(
-                "localhost", "MOngoDB", "s3cret".toCharArray());*//*
-        MongoDatabase mongoDatabase = mongo.getDatabase("db1");
-        MongoCollection mongoCollection = mongoDatabase.getCollection("accounts");
-        //connection = DriverManager.getConnection("mongodb://localhost:27017/local?authSource=admin");
-        System.out.println(mongoCollection);
-    }*/
 
     private Conexion() throws SQLException, IOException {
         MongoClient mongo = new MongoClient("localhost", 27017);
@@ -33,10 +20,7 @@ public class Conexion {
                 "localhost", "MOngoDB", "s3cret".toCharArray());*/
         MongoDatabase mongoDatabase = mongo.getDatabase("db1");
         MongoCollection mongoCollection = mongoDatabase.getCollection("accounts");
-        //connection = DriverManager.getConnection("mongodb://localhost:27017/local?authSource=admin");
         System.out.println(mongoCollection);
-
-
     }
 
 
@@ -74,16 +58,7 @@ public class Conexion {
 
     // probamos la conexion
     public static void main(String[] args) throws SQLException, IOException {
-        //pruebaConexion();
         Conexion conexion = new Conexion();
         System.out.println(conexion);
-        /*try {
-            System.out.println("conexión: " + Conexion.getInstance().connection);
-            System.out.println("conexión: " + Conexion.getInstance().connection);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
     }
 }
